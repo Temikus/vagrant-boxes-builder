2926dc9e527ce976709012885d46f92d92755270
Artem Yakimenko <code@temik.me>
2014-11-30 13:05:11 +0100

    CentOS7 uses biosdevname by default, adjusting accordingly.

--- packer/scripts/cleanup.sh
+++ packer/scripts/cleanup.sh
@@ -4,5 +4,4 @@ rm -rf /etc/yum.repos.d/{puppetlabs,epel}.repo
 rm -rf VBoxGuestAdditions_*.iso
 
 # Remove traces of mac address from network configuration
-sed -i /HWADDR/d /etc/sysconfig/network-scripts/ifcfg-eth0
-rm /etc/udev/rules.d/70-persistent-net.rules
+sed -i /HWADDR/d /etc/sysconfig/network-scripts/ifcfg-en*
